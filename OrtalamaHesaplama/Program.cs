0694f0ae0b6e6f0f8b159561acf7abd13d3d9b89
Seher Turmuş <99262737+SeherTurmus@users.noreply.github.com>
2023-01-29 14:17:02 +0300

    first  commit

---
+++ OrtalamaHesaplama/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace OrtalamaHesaplama
+{
+    public class Program
+    {        
+        static void Main()
+        {
+            Console.WriteLine("Lütfen Fibonacci derinliğini girin : ");
+
+            var num = decimal.Parse(Console.ReadLine());
+            var sum = Fib(num);
+
+            var ave = Math.Round(sum / (num + 1), 3);
+            Console.WriteLine($"Ortalama : {ave}");
+        }
+        public static int Fib(decimal n)
+        {
+            if(n == 0 || n == 1)
+                return 1;
+            return Fib(n-1) + Fib(n-2);
+        }
+    }    
+}

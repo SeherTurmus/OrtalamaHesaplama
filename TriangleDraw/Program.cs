51c23d1b694b4081d98e989f38f803ed4cdfabee
Seher Turmuş <99262737+SeherTurmus@users.noreply.github.com>
2023-01-29 23:39:23 +0300

    Added the triangle draw folder

---
+++ TriangleDraw/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TriangleDraw
+{
+    public class Program
+    {
+        static void Main()
+        
+        {
+            var x = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("*");
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
